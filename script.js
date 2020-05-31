79ed838ff922d3e6ced61120222a28c3c4ca4053
Trisha :) <trigan141@fusdk12.net>
2020-05-31 03:01:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+//NOTE: The nav bar has an about and contact section that will be implimented for the final project. Since this is just a draft, I only put in the requirements keeping in mind the scope of the porject and the limited time I have had available.
+
+$('#1').click(function() {
+  window.open("https://codepen.io/Peachs/full/POOdYm/",'_blank');
+});
+$('#2').click(function() {
+ window.open("https://docs.google.com/document/d/1ApRylJ45ForfI8P5NA4ZkPsnSKSLXaBhic16uOrjhVA/edit?usp=sharing",'_blank');
+});
+$('#3').click(function() {
+ window.open("https://scratch.mit.edu/projects/173187375/",'_blank');
+});
+$('#4').click(function() {
+ window.open("https://scratch.mit.edu/projects/174385803/",'_blank');
+});
+$('#5').click(function() {
+ window.open("https://drive.google.com/file/d/0B0dnMD_sF8cqek11cTRIa2d3bEk/view?usp=sharing",'_blank');
+});
+$('#6').click(function() {
+ window.open("https://docs.google.com/document/d/1jYBu92y6gRSPEyXRPRK030Wyi0QsPrDhk-vSeEImxPg/edit?usp=sharing",'_blank');
+});
+$('#7').click(function() {
+ window.open("https://earsketch.gatech.edu/earsketch2/#?sharing=0dSiBRkRzIWNj7XB2DRc7Q",'_blank');
+});
+$('#8').click(function() {
+ window.open("https://codepen.io/Peachs/full/rzZrQM/",'_blank');
+});
+$('#9').click(function() {
+ window.open("https://codepen.io/Peachs/full/VMMomo/",'_blank');
+});
+$('#10').click(function() {
+ window.open("https://codepen.io/Peachs/pen/QOwWjR?editors=1111",'_blank');
+});
+$('#11').click(function() {
+ window.open("https://codepen.io/Peachs/full/BJaYXo/",'_blank');
+});
+$('#12').click(function() {
+ window.open("https://earsketch.gatech.edu/earsketch2/#?sharing=Py7IQztarfHgR8z3T-9VZg",'_blank');
+});
+$('#13').click(function() {
+ window.open("https://studio.code.org/projects/applab/2kkiKRmDr3Mog6EjKj-DaQlqCtWhWCtsYKBHWeRudZE",'_blank');
+});
+$('#14').click(function() {
+ window.open("https://codepen.io/Peachs/full/opdxWE/",'_blank');
+});
+$('#15').click(function() {
+ window.open("https://drive.google.com/file/d/1CdR3GJDHlMsDFI_b59yDv6LyRL2Xn09f/view?usp=sharing",'_blank');
+});
+$('#16').click(function() {
+ window.open("https://drive.google.com/file/d/1h-6YmQuKJ67-zONSGK5H-ktJzV0p_7ij/view?usp=sharing",'_blank');
+});
+$('#17').click(function() {
+ window.open("",'_blank');
+});
